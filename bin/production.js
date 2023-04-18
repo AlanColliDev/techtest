debe7658d18b8ba7a2d05ff221dec41b0477ce13
AlanColliDev <alancollidesarrollo@outlook.com>
2023-04-18 02:45:14 -0600

    fix production.js

--- bin/production.js
+++ bin/production.js
@@ -1,1 +1,5 @@
-require('../src/index.js');
+// require('../src/index.js');
+
+import index from '../dist/index.js';
+
+index
--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ const { Sequelize } = require('sequelize');
 
 server.listen(server.get('PORT'), async function() {
 	console.log('HI! IM WORKING ON PORT...', this.address().port)
-	
 });
 
