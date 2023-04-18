0c6ca9bd44a03b3bf6eb7f9083c6337881a63670
AlanColliDev <alancollidesarrollo@outlook.com>
2023-04-18 01:06:34 -0600

    fix npm command

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import alumnosRouter from './routes/alumnos.routes'
+import config from './config'
 import cors from 'cors';
 
 const bodyParser = require('body-parser');
@@ -8,7 +9,7 @@ const server = express();
 server.use(cors());
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
-server.set('PORT', 53445);
+server.set('PORT', config);
 
 server.use(alumnosRouter);
 
