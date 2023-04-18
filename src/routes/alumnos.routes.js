1b4248e15ff027877798b3ee946e347c1704bda9
AlanColliDev <alancollidesarrollo@outlook.com>
2023-04-18 01:21:18 -0600

    add route test

--- src/routes/alumnos.routes.js
+++ src/routes/alumnos.routes.js
@@ -5,6 +5,11 @@ const router = Router();
 router.get('/alumno', getAll);
 router.post('/alumno', createNew);
 router.put('/alumno', updateAlumno);
+router.get('/test', (req, resp) => {
+	resp.status(200).json({
+		message: "hello from NODEJS"
+	})
+});
 
 
 export default router;	
